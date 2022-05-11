bc2bece60fff66b32d8b59636f44bbd834e2ded3
ArcticRug25 <wyw82839148@163.com>
2022-05-12 00:36:27 +0800

    实现 effect 的 stop 功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,5 +1,10 @@
+import { extend } from "../shared";
+
 class ReactiveEffect {
   private _fn: any;
+  deps = [];
+  active = true;
+  onStop? : () => void;
   constructor(fn, public scheduler?) {
     this._fn = fn;
   }
@@ -7,6 +12,21 @@ class ReactiveEffect {
     activeEffect = this;
     return this._fn()
   }
+  stop() {
+    if (this.active) {
+      cleanupEffect(this);
+      if (this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  })
 }
 
 const targetMap = new Map();
@@ -26,7 +46,10 @@ export function track(target, key) {
     depsMap.set(key, dep);
   }
 
+  if (!activeEffect) return;
+
   dep.add(activeEffect);
+  activeEffect.deps.push(dep);
 }
 
 export function trigger(target, key) {
@@ -46,8 +69,17 @@ let activeEffect;
 export function effect(fn, options: any = {}) {
   // fn
   const _effect = new ReactiveEffect(fn, options.scheduler);
+  // extend
+  extend(_effect, options);
 
   _effect.run();
 
-  return _effect.run.bind(_effect);
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect; 
+
+  return runner;
+}
+
+export function stop(runner) {
+  runner.effect.stop();
 }
